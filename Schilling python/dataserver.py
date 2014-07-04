745fd7198b07cc65b0f7ced5171f8803247f6ce7
Hampus Paulsson <Hampus@paulsson.net>
2014-07-04 13:28:22 +0200

    Lade till serversaker

---
+++ Schilling python/dataserver.py
@@ -0,0 +1,34 @@
+import tornado.ioloop
+import tornado.web
+import tornado.escape
+import json
+import urllib
+
+"""
+Ett enkelt program dar man kan skriva in data i tva textboxar.
+Datan i dessa tva skickas med en POST-request som hanteras i DataHandler.
+
+Detta program kan anvandas som en grund till att mata in data till 
+delta-v-programmet.
+
+"""
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('dataform.html')  # Renderar formularet
+        self.set_header("Content-Type", "application/json; utf-8")
+
+class DataHandler(tornado.web.RequestHandler):
+    def post(self):
+        self.render('calc.html')    # Renderar en testsida 
+        params = self.request.arguments # Parametrarna fran formularet
+        print params
+        
+application = tornado.web.Application([
+    (r"/", MainHandler),
+    (r"/calc", DataHandler),
+])
+
+application.listen(8010)
+tornado.ioloop.IOLoop.instance().start()
