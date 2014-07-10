8bfea32f7b12c47e6300d9c04f72fa4e676651f2
hanserikbratt <hebratt@gmail.com>
2014-07-10 10:53:49 +0200

    en variant av testingnumpy som gör en inclinationchange och plottar dessutom jorden

--- testingnumpy.py
+++ testingnumpy.py
@@ -10,11 +10,11 @@ def gravForce(m,r):
 	F = (-consts.G*M*m/(normr**3))*r
 	return F
 
-
-r = (6371e3 + 200000)*np.array([1.,0,0])
+re = 6371e3
+r = (re + 300000)*np.array([1.,0,0])
 m = 9000
-steps = 100.
-ttot = 5000.
+steps = 1000.
+ttot = 15000.
 t = 0
 dt = ttot/steps
 i = 0
@@ -23,17 +23,16 @@ rsave = np.empty([steps,3])
 vsave = np.empty([steps,3])
 
 while (i < steps) :
-	if (i>0.7*steps):										#initiate burn at 70% of elapsed test time
-		a = gravForce(m,r)/m +2*v/(math.sqrt(np.dot(v,v)))
+	if (t>3500 and t<3600):										#initiate burn at 70% of elapsed test time
+		a = gravForce(m,r)/m +20*np.array([0.,0.,1.])#10*v/(math.sqrt(np.dot(v,v)))
 	else:
 		a = gravForce(m,r)/m
 	
-	print a
+
 	v = v + a*dt
-	print v
+
 	r = r + v*dt
-	print r
-	print ""
+
 	rsave[i,:] = r 
 	vsave[i,:] = v 
 	t = t + dt
@@ -42,15 +41,17 @@ while (i < steps) :
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-"""
-ax.set_zlim(bottom=-250000, top=250000)
-ax.set_ylim(bottom=-250000, top=250000)
-ax.set_xlim(left=-250000, right=250000)
-"""
-x, y, z = np.meshgrid(np.arange(-300000, 1000, 300000),
-                      np.arange(-300000, 1000, 300000),
-                      np.arange(-300000, 1000, 300000))
 
-print rsave[:,0]
-ax.scatter(rsave[:,0],rsave[:,1],rsave[:,2])
+ax.set_zlim(bottom=-(re + 320000), top=(re + 320000))
+ax.set_ylim(bottom=-(re + 320000), top=(re + 320000))
+ax.set_xlim(left=-(re + 320000), right=(re + 320000))
+
+theta = np.linspace(0.,2*math.pi,100)
+phi = np.linspace(0.,math.pi,100)
+x = re * np.outer(np.cos(theta), np.sin(phi))
+y = re * np.outer(np.sin(theta), np.sin(phi))
+z = re * np.outer(np.ones(np.size(theta)), np.cos(phi))
+
+ax.scatter(rsave[:,0],rsave[:,1],rsave[:,2],s=2,c='r')
+ax.plot_surface(x,y,z)
 plt.show()
