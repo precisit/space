d919fcb9da3d011e91c848876bbd66e636f98c0e
hanserikbratt <hebratt@gmail.com>
2014-07-31 17:21:45 +0200

    Restifierat atmosfärsfunktioner

---
+++ simulation2/RESTAPIs/server.py
@@ -0,0 +1,90 @@
+import tornado.ioloop
+import tornado.web
+import tornado.log
+import logging
+import json
+import mainschilling
+import mainrocketsim
+import sys
+sys.path.append('C:\Github\space\Schilling python')
+import mainschilling
+
+log = logging.getLogger("tornado.general")
+tornado.log.enable_pretty_logging();
+
+class PingHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write('pong')
+
+
+class TestjsHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('test.html')
+
+class DeltaVHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*") #allow requests from other domains than self
+
+    def post(self):
+        indata = json.loads(self.request.body)
+        
+        #indata.firstStage.wetMass #From meeting
+        response = {}
+        try:
+            response['dVtot'], response['Approximations'] = mainschilling.deltaV(indata)
+        except KeyError, e:
+            raise tornado.web.HTTPError(400,'Not enough input data -- missing: "%s"' % str(e))
+        self.write(json.dumps(response))
+
+class TmixHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*") #allow requests from other domains than self
+
+    def post(self):
+        indata = json.loads(self.request.body)
+        response = {}
+        try:
+            response["Tmix"], response['Approximations'] = mainschilling.Tmix(indata)
+        except KeyError, e:
+            raise tornado.web.HTTPError(400,'Not enough input data -- missing: "%s"' % str(e))
+        self.write(json.dumps(response))
+
+class RocketCapabilityHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*") #allow requests from other domains than self
+
+    def post(self):
+        indata = json.loads(self.request.body)
+        response = {}
+        try:
+            response["mp"], response['Approximations'] = mainschilling.mpSolver(indata)
+        except KeyError, e:
+            raise tornado.web.HTTPError(400,'Not enough input data -- missing: "%s"' % str(e))
+        self.write(json.dumps(response))
+
+class AtmoPressureHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*") #allow requests from other domains than self
+
+    def post(self):
+        indata = json.loads(self.request.body)
+        response = {}
+        try:
+            response["pressure"], response['altitude'] = mainrocketsim.pressure(indata)
+        except KeyError, e:
+            raise tornado.web.HTTPError(400,'Not enough input data -- missing: "%s"' % str(e))
+        self.write(json.dumps(response))
+
+application = tornado.web.Application([
+    (r"/ping", PingHandler),
+    (r"/testjs",TestjsHandler),
+    (r"/deltaV",DeltaVHandler),
+    (r"/Tmix",TmixHandler),
+    (r"/rocketCapability",RocketCapabilityHandler),
+    (r"/atmoPressure",AtmoPressureHandler)
+
+], autoreload=True)
+
+#TODO: https on prod
+application.listen(8020)
+tornado.ioloop.IOLoop.instance().start()
