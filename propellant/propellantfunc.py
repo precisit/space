147a0de30cdd54287dd69378acdde7709eec654e
hanserikbratt <hebratt@gmail.com>
2014-08-06 15:10:19 +0200

    fixade REST för propellant med mera...
    ...skapade funktioner för metan. skapade en ny serverfil i /space.
    Ändrade sökvägen till tck-filer till absoluta sökvägar. Lade till
    __init__.py filer i mappar (av någon anledning).

--- propellant/propellantfunc.py
+++ propellant/propellantfunc.py
@@ -4,48 +4,87 @@ from scipy import interpolate
 import matplotlib.pyplot as plt
 import pickle
 
-OKRtck = pickle.load(open('OK-R.pk1'))
-OKMtck = pickle.load(open('OK-M.pk1'))
-OKTtck = pickle.load(open('OK-T.pk1'))
-OKKtck = pickle.load(open('OK-K.pk1'))
+OKRtck = pickle.load(open('C:/Github/space/propellant/OK-R.pk1'))
+OKMtck = pickle.load(open('C:/Github/space/propellant/OK-M.pk1'))
+OKTtck = pickle.load(open('C:/Github/space/propellant/OK-T.pk1'))
+OKKtck = pickle.load(open('C:/Github/space/propellant/OK-K.pk1'))
+OMRtck = pickle.load(open('C:/Github/space/propellant/OM-R.pk1'))
+OMMtck = pickle.load(open('C:/Github/space/propellant/OM-M.pk1'))
+OMTtck = pickle.load(open('C:/Github/space/propellant/OM-T.pk1'))
+OMKtck = pickle.load(open('C:/Github/space/propellant/OM-K.pk1'))
 
-def OMRloxker(Pc,Pe):
-	return OKRtck(Pc,Pe)
+def OMR(params):
+	if params["fuel"].lower() == 'loxker':
+		return OKRtck(params["Pc"],params["Pe"])
+	elif params["fuel"].lower() == 'loxmeth':
+		return OMRtck(params["Pc"],params["Pe"])
+	else:
+		return "fuel not found"
 
-def AFTloxker(Pc,Pe):
-	return OKTtck(Pc,Pe)
+def AFT(params):
+	if params["fuel"].lower() == 'loxker':
+		return OKTtck(params["Pc"],params["Pe"])
+	elif params["fuel"].lower() == 'loxmeth':
+		return OMTtck(params["Pc"],params["Pe"])
+	else:
+		return "fuel not found"
 
-def GMWloxker(Pc,Pe):
-	return OKMtck(Pc,Pe)
+def GMW(params):
+	if params["fuel"].lower() == 'loxker':
+		return OKMtck(params["Pc"],params["Pe"])
+	elif params["fuel"].lower() == 'loxmeth':
+		return OMMtck(params["Pc"],params["Pe"])
+	else:
+		return "fuel not found"
 
-def SHRloxker(Pc,Pe):
-	return OKKtck(Pc,Pe)
+def SHR(params):
+	if params["fuel"].lower() == 'loxker':
+		return OKKtck(params["Pc"],params["Pe"])
+	elif params["fuel"].lower() == 'loxmeth':
+		return OMKtck(params["Pc"],params["Pe"])
+	else:
+		return "fuel not found"
 
+def combData(params):
+	if params["fun"].lower() == "omr":
+		result = OMR(params)
+
+	elif params["fun"].lower() == "aft":
+		result = AFT(params)
+
+	elif params["fun"].lower() == "gmw":
+		result = GMW(params)
+
+	elif params["fun"].lower() == "shr":
+		result = SHR(params)
+
+	return result.tolist(), params["Pc"], params["Pe"]
 
 if __name__ == '__main__':
 
-	x=np.linspace(0,250,100)
-	plt.plot(x,OMRloxker(x,0.1))
-	plt.show()
+	values = np.zeros([2,4,100,10])
+	Pe = np.zeros([2,2,10])
+	Pe[:,0,:] = np.linspace(0.1,1,10)
+	Pe[0,1,:] = np.linspace(2.2,2.4,10)
+	Pe[1,1,:] = np.linspace(2.7,2.9,10)
+	x = np.linspace(0,250,100)
+	for i in range(100):
+		for j in range(10):
 
-	print OMRloxker(0,0)
-	print OMRloxker(100,1)
+			values[0,0,i,j] = OMR({"Pc":x[i],"Pe":Pe[0,0,j],"fuel":"loxker"})
+			values[1,0,i,j] = OMR({"Pc":x[i],"Pe":Pe[1,0,j],"fuel":"loxmeth"})
+			values[0,1,i,j] = AFT({"Pc":x[i],"Pe":Pe[0,1,j],"fuel":"loxker"})
+			values[1,1,i,j] = AFT({"Pc":x[i],"Pe":Pe[1,1,j],"fuel":"loxmeth"})
+			values[0,2,i,j] = GMW({"Pc":x[i],"Pe":Pe[0,1,j],"fuel":"loxker"})
+			values[1,2,i,j] = GMW({"Pc":x[i],"Pe":Pe[1,1,j],"fuel":"loxmeth"})
+			values[0,3,i,j] = SHR({"Pc":x[i],"Pe":Pe[0,1,j],"fuel":"loxker"})
+			values[1,3,i,j] = SHR({"Pc":x[i],"Pe":Pe[1,1,j],"fuel":"loxmeth"})
 
-	plt.plot(x,AFTloxker(x,2.2))
-	plt.show()
-
-	print AFTloxker(0,0)
-	print AFTloxker(125,2.3)
-
-	x=np.linspace(0,250,100)
-	plt.plot(x,GMWloxker(x,2.3))
-	plt.show()
-
-	print GMWloxker(0,0)
-	print GMWloxker(125,2.4)
-
-	plt.plot(x,SHRloxker(x,2.2))
-	plt.show()
-
-	print SHRloxker(0,0)
-	print SHRloxker(75,2.2)
+	for i in range(4):
+		for j in range(10):
+			plt.plot(x,values[0,i,:,j])
+		plt.show()
+	for i in range(4):
+		for j in range(10):
+			plt.plot(x,values[1,i,:,j])
+		plt.show()
